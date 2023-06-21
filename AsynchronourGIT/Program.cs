a65fbc0c1d0565a3309a834c4522ff652d23d21e
suseelrajap <suseelrajap@DESKTOP-Q4IBAE5>
2023-06-21 17:14:10 +0530

    sixthcommit

--- AsynchronourGIT/Program.cs
+++ AsynchronourGIT/Program.cs
@@ -53,5 +53,17 @@ class Program
         return 20;
     }
 
+    static async Task<int> LongProcess6()
+    {
+        Console.WriteLine("LongProcess 6 Started");
+
+        await Task.Delay(4000); // hold  4 seconds
+
+        Console.WriteLine("LongProcess 6 Completed");
+
+        return 20;
+    }
+
+
 
 }
