a5b17003ef7877b20a32a5d293dbe568b88efab6
madhusuthanan.m@smodeguntagmail.onmicrosoft.com <madhusuthanan.m@smodeguntagmail.onmicrosoft.com>
2023-06-21 16:15:10 +0530

    first commit

---
+++ AsynchronourGIT/Program.cs
@@ -0,0 +1,46 @@
+﻿
+class Program
+{
+
+
+    static async Task Main(string[] args)
+    {
+        var result1 = await LongProcess1();
+
+
+         
+
+        var result2 = LongProcess2();
+
+
+        Console.WriteLine("After two long processes.");
+
+         
+
+        Console.ReadKey();
+    }
+
+    static async Task<int> LongProcess1()
+    {
+        Console.WriteLine("LongProcess 1 Started");
+
+        await Task.Delay(4000);
+
+        Console.WriteLine("LongProcess 1 Completed");
+
+        return 67;
+    }
+
+    static async Task<int> LongProcess2()
+    {
+        Console.WriteLine("LongProcess 2 Started");
+
+        await Task.Delay(4000); // hold  4 seconds
+
+        Console.WriteLine("LongProcess 2 Completed");
+
+        return 20;
+    }
+
+    
+}
